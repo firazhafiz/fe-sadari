8dc9751cb7796935cb8e8633f26e51f59f217fb0
firazhafiz <firazfulvianhafiz05@gmail.com>
2025-08-27 20:51:58 +0700

    add sticky nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { HeroScrollDemo } from "@/components/organisms/Hero";
-import Header from "@/components/organisms/Header";
 import Gejala from "@/components/organisms/Gejala";
 import ScrollButton from "@/components/moleculs/ScrollButton";
 import About from "@/components/organisms/About";
@@ -7,12 +6,15 @@ import Tips from "@/components/organisms/Tips";
 import TestimoniLeft from "@/components/organisms/TestimoniLeft";
 import TestimoniRight from "@/components/organisms/TestimoniRight";
 import TestimoniTitle from "@/components/atoms/TestimoniTitle";
+import Navbar from "@/components/moleculs/Navbar";
+import BannerHeader from "@/components/moleculs/BannerHeader";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white">
+    <main className="min-h-screen bg-white pt-22">
       <section>
-        <Header />
+        <Navbar />
+        <BannerHeader />
         <ScrollButton />
       </section>
       <section>
