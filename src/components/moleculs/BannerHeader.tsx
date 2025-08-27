89aaf19c501580ffa6379b41f2b57464f96b77ff
Ilham <mi1797128@gmail.com>
2025-08-27 23:18:54 +0700

    feat: responsive banner dan navigasi

--- src/components/moleculs/BannerHeader.tsx
+++ src/components/moleculs/BannerHeader.tsx
@@ -1,13 +1,8 @@
 export default function BannerHeader() {
   return (
     <div className="w-full relative bg-navy text-white">
-      <div
-        className="mx-auto w-full max-w-7xl px-4 text-center tracking-[0.3em]"
-        style={{ paddingTop: "15px", paddingBottom: "15px" }}
-      >
-        <span className="text-[12px] md:text-sm font-semibold">
-          KENALI GEJALA DAN CEGAH HIPERTENSI SEJAK DINI
-        </span>
+      <div className="mx-auto w-full max-w-7xl px-4 text-center tracking-[1px] md:tracking-[0.3em]" style={{ paddingTop: "15px", paddingBottom: "15px" }}>
+        <span className="text-[12px] md:text-sm font-semibold ">KENALI GEJALA DAN CEGAH HIPERTENSI SEJAK DINI</span>
       </div>
     </div>
   );
