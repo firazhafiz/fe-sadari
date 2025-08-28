0827aba129760317fee4206ec0d07c846946d476
firazhafiz <firazfulvianhafiz05@gmail.com>
2025-08-28 10:32:42 +0700

    rev warna hero dan title tes

--- src/components/moleculs/BannerHeader.tsx
+++ src/components/moleculs/BannerHeader.tsx
@@ -1,8 +1,13 @@
 export default function BannerHeader() {
   return (
-    <div className="w-full relative bg-navy text-white">
-      <div className="mx-auto w-full max-w-7xl px-4 text-center tracking-[1px] md:tracking-[0.3em]" style={{ paddingTop: "15px", paddingBottom: "15px" }}>
-        <span className="text-[12px] md:text-sm font-semibold ">KENALI GEJALA DAN CEGAH HIPERTENSI SEJAK DINI</span>
+    <div className="w-full relative bg-cream text-slate-800">
+      <div
+        className="mx-auto w-full max-w-7xl px-4 text-center tracking-[1px] md:tracking-[0.3em]"
+        style={{ paddingTop: "15px", paddingBottom: "15px" }}
+      >
+        <span className="text-[12px] md:text-sm font-semibold ">
+          KENALI GEJALA DAN CEGAH HIPERTENSI SEJAK DINI
+        </span>
       </div>
     </div>
   );
